bc07e8863bea8d652133e4ee132368ac7b6b40a9
1DAW-josecor475 <jcorfer910@g.educaand.es>
2024-11-22 14:05:12 +0100

    Creado dos nuevos sistemas, eventos y reorganizacion de la estructura

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,7 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const sistemaSolar = document.getElementById("sistema-solar");
+    const sistemaProxmox = document.getElementById("sistema-proxmox");
+    const sistemaLinux = document.getElementById("sistema-linux");
+    const sistemaShinChan = document.getElementById("sistema-shinchan");
 
-    sistemaSolar.addEventListener("click", () => {
-        sistemaSolar.classList.toggle("centrado");
+    sistemaProxmox.addEventListener("click", () => {
+        sistemaProxmox.classList.toggle("centrado");
+    });
+    sistemaLinux.addEventListener("click", () => {
+        sistemaLinux.classList.toggle("centrado");
+    });
+    sistemaShinChan.addEventListener("click", () => {
+        sistemaShinChan.classList.toggle("centrado");
     });
 });
